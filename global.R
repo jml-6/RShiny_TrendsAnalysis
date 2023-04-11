086f4faf5f87661264434f28e5b821f104eeabc5
John Linford <116912043+jml-6@users.noreply.github.com>
2023-04-11 17:43:38 -0600

    Update global.R

---
+++ global.R
@@ -0,0 +1,41 @@
+library(tidyverse)
+library(data.table)
+library(shiny)
+library(shinythemes)
+library(shinydashboard)
+library(plotly)
+library(bslib)
+library(feather)
+library(DT)
+
+
+load('data/unique_cpc_group.Rdata')
+assignee <- read.csv('data/g_assignee_disambiguated_2012_2021.feather')
+location <- read.csv('data/g_location_disambiguated_2012_2021.feather')
+patent <- read.csv('data/g_patent_2012_2021.feather')
+cpc <- fread('data/g_cpc_current_2012_2021.csv')
+cpc$patent_id <- as.character(cpc$patent_id)
+
+# Resources
+# https://shiny.rstudio.com/
+# https://shiny.rstudio.com/gallery/
+# Icons https://fontawesome.com/search?o=r&m=free
+# HTML tags https://shiny.rstudio.com/articles/tag-glossary.html
+# Inputs https://shiny.rstudio.com/tutorial/written-tutorial/lesson3/
+
+# Agenda:
+# Server
+# Reactivity
+# Reactive values
+# Inputs
+# Server logic
+# Rendering outputs
+# Displaying in UI
+# Observers
+# Debugging - browser()
+# Work together in class:
+# Add code from competetive positioning assignemnt to make your app interactive
+
+
+
+
