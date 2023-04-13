6f5b6905374e855773a62e29b98fc6125d45139c
Ethan Hollis <e.hollis32@gmail.com>
2023-04-13 16:17:35 -0600

    asdfa

--- server.R
+++ server.R
@@ -10,91 +10,48 @@ server <- function(input,output,session) {
   # and use reactiveVal or reactiveValues when you have an object that you want to maintain a state
   
   # # Reactive objects for trends:
-  text_labels_input <- reactive({
-    req(input$trends_input1)
-    paste('You entered: ',input$trends_input1,sep = '',collapse = '')
+  cpc_codes <- reactive({
+    req(input$cpc_codes)
      })
-   output$trends_output1 <- renderText({text_labels_input()})
-   
+
    text_labels_input1 <- reactive({
      req(input$trends_input2)
-     paste('You entered: ',input$trends_input2,sep = '',collapse = '')
    })
-   output$trends_output2 <- renderText({text_labels_input1()})
-  
+
     text_labels_input2 <- reactive({
      req(input$trends_input3)
-     paste('You entered: ',input$trends_input3,sep = '',collapse = '')
    })
-   output$trends_output3 <- renderText({text_labels_input2()})
-   
+
    # # Reactive objects for comp:
    text_labels_input3 <- reactive({
      req(input$comp_input1)
-     paste('You entered: ',input$comp_input1,sep = '',collapse = '')
-   })
-   output$comp_output1 <- renderText({text_labels_input3()})
-   
-   text_labels_input4 <- reactive({
-     req(input$comp_input2)
-     paste('You entered: ',input$comp_input2,sep = '',collapse = '')
-   })
-   output$comp_output2 <- renderText({text_labels_input4()})
-   
-   text_labels_input5 <- reactive({
-     req(input$comp_input3)
-     paste('You entered: ',input$comp_input3,sep = '',collapse = '')
-   })
-   output$comp_output3 <- renderText({text_labels_input5()})
-   
-   randomVals <- eventReactive(input$go, {
-     runif(input$n)
-   })
-   
-   output$plot <- renderPlot({
-     hist(randomVals())
    })
 
-  
-  # # Reactive objects:
-  # market_cpc_input_text <- reactive({
-  #   req(input$market_cpcs_input)
-  #   paste(input$market_cpcs_input,sep = '',collapse = ', ')
-  #   })
-  # output$market_cpc_output <- renderText({market_cpc_input_text()})
-  
-  
-  # Reactive values and observers
-  # r_list <- reactiveValues(txt = 'You entered: ',txt2 = 'You typed: ')
-  # observe({
-  #   output$text_labels_output <- renderText({
-  #     paste(r_list$txt, input$submarket_labels_input,sep = '',collapse = '')
-  #   })
-  #   })
-  
-  
-  # # ObserveEvent
-  # r_list <- reactiveValues(txt = 'You entered: ',txt2 = 'You typed: ')
-  # observeEvent(input$generate_competitive_positioning,{
-  #   r_list$txt <- paste(r_list$txt, input$submarket_labels_input,sep = '',collapse = '')
-  # })
-  # output$text_labels_output <- renderText({
-  #   r_list$txt
-  # })
-  
-  
-  # # Debugging - browser()
-  #   r_list <- reactiveValues(txt = 'You entered: ',txt2 = 'You typed: ')
-  #   observeEvent(input$generate_competitive_positioning,{
-  #     browser()
-  #     temp <- 1
-  #     print(temp*5)
-  #     r_list$txt <- paste(r_list$txt, input$submarket_labels_input,sep = '',collapse = '')
-  # 
-  #   })
-  #   output$text_labels_output <- renderText({
-  #     r_list$txt
-  #   })
+   text_labels_input4 <- reactive({
+     req(input$comp_input2)
+   })
+
+   text_labels_input5 <- reactive({
+     req(input$comp_input3)
+   })
+   
+   competition <- reactiveValues(dt=data.frame(),plot = plotly_empty())
+
+   ##Here we create our output and stuff
+   observeEvent(input$go,{
+     dt <- cpc %>% filter(grepl(pattern = paste(input$cpc_codes,sep ='',collapse='|'),x = cpc$cpc_group,ignore.case = T))
+     dt <- merge(dt,patent,by = 'patent_id')
+     # merge with assignee
+     dt <- merge(dt,assignee,by = 'patent_id') # why is this dropping? no assignee?
+     competition$dt <- head(dt)
+     
+     output$competiton_dt <- renderDataTable({competition$dt})
+     
+
+     
+   })
+     
+
   
   
 }
